c60f83a6d9a4872ec7e376109e65c34a26deca57
Simon Fuhrmann <simonfuhrmann@gmx.de>
2017-11-17 20:21:41 -0800

    Organize elements into directories mixins/ and common/

--- src/diy-user-recipes.js
+++ src/diy-user-recipes.js
@@ -2,6 +2,23 @@ class DiyUserRecipes extends Polymer.Element {
   static get is() {
     return 'diy-user-recipes';
   }
+
+  static get properties() {
+    return {
+      testvalue: {
+        type: String,
+        observer: 'onValueChanged_',
+      }
+    };
+  }
+
+  onValueChanged_(value) {
+    console.log('value', value);
+  }
+
+  onSaveTap_() {
+    this.set('testvalue', "asdfasdf 123");
+  }
 }
 
 customElements.define(DiyUserRecipes.is, DiyUserRecipes);
