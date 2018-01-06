366d5f0b6329463c09832d41056d41d40c570022
Simon Fuhrmann <simonfuhrmann@gmx.de>
2018-01-06 02:20:15 -0800

    Update welcome page

--- src/routes/diy-welcome.js
+++ src/routes/diy-welcome.js
@@ -1,37 +1,10 @@
-class DiyWelcome extends DiyMixinRedux(Polymer.Element) {
+class DiyWelcome extends Polymer.Element {
   static get is() {
     return 'diy-welcome';
   }
+
   static get properties() {
-    return {
-      welcomeMessage: String,
-      userDetails: {
-        type: Object,
-        statePath: 'user.auth',
-        observer: 'onUserAuthChanged_',
-      },
-    };
-  }
-
-  ready() {
-    super.ready();
-    this.onUserAuthChanged_();
-  }
-
-  onUserAuthChanged_(userAuth) {
-    const message = 'Welcome to Flavortopia!';
-    if (!userAuth || !userAuth.signedIn) {
-      this.set('welcomeMessage', message);
-      return;
-    }
-
-    const user = userAuth.firebaseUser;
-    if (!user.displayName) {
-      this.set('welcomeMessage', message);
-      return;
-    }
-
-    this.set('welcomeMessage', 'Welcome, ' + user.displayName + '!');
+    return {};
   }
 }
 
