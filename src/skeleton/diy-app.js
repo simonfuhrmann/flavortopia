7e44d4afc66360c5d529827e25f6501e7c3e6724
Simon Fuhrmann <simonfuhrmann@gmx.de>
2018-01-06 14:04:46 -0800

    Improved mobile experience, overhauled app-layout

--- src/skeleton/diy-app.js
+++ src/skeleton/diy-app.js
@@ -2,6 +2,17 @@ class DiyApp extends DiyMixinRouter(Polymer.Element) {
   static get is() {
     return 'diy-app';
   }
+
+  toggleDrawer_() {
+    if (this.$.drawer.persistent) {
+      return;
+    }
+    if (this.$.drawer.opened) {
+      this.$.drawer.close();
+    } else {
+      this.$.drawer.open();
+    }
+  }
 }
 
 customElements.define(DiyApp.is, DiyApp);
