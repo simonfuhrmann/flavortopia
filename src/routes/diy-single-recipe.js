6e070d830c626d8b86b6cc6da88ed84241a22268
Simon Fuhrmann <simonfuhrmann@gmx.de>
2018-01-22 23:04:45 -0800

    Add single recipe route element

---
+++ src/routes/diy-single-recipe.js
@@ -0,0 +1,42 @@
+class DiySingleRecipe extends DiyMixinRouter(Polymer.Element) {
+  static get is() {
+    return 'diy-single-recipe';
+  }
+
+  static get properties() {
+    return {
+      isLoading: {
+        type: Boolean,
+        value: false,
+      },
+      loadingError: {
+        type: String,
+        value: null,
+      },
+      recipe: {
+        type: Object,
+        value: null,
+      },
+    };
+  }
+
+  static get observers() {
+    return [
+      'updateRoute_(activeRoute)'
+    ];
+  }
+
+  updateRoute_(activeRoute) {
+    const query = activeRoute.query || { id: '' };
+    if (!query.id) {
+      this.set('loadingError', 'No recipe ID given');
+      return;
+    }
+
+    this.set('recipe', null);
+    const recipeKey = query.id;
+    this.$.firebaseGet.subscribeSingleRecipe(recipeKey);
+  }
+}
+
+customElements.define(DiySingleRecipe.is, DiySingleRecipe);
