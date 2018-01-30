8efdb0d082cd60d7638cae7190569c1587b9a2b1
Simon Fuhrmann <simonfuhrmann@gmx.de>
2018-01-29 20:53:52 -0800

    Add Real Flavors (RF) and Super Concentrates (SC) data files

---
+++ src/data/flavors-rf.js
@@ -0,0 +1,942 @@
+const staticDataFlavorsRf = {
+  "rf-aged-bourbon-cream": {
+    "name": "Aged Bourbon Cream",
+    "vendor": "rf"
+  },
+  "rf-almond": {
+    "name": "Almond",
+    "vendor": "rf"
+  },
+  "rf-apple": {
+    "name": "Apple",
+    "vendor": "rf"
+  },
+  "rf-apple-butter": {
+    "name": "Apple Butter",
+    "vendor": "rf"
+  },
+  "rf-apple-crumble": {
+    "name": "Apple Crumble",
+    "vendor": "rf"
+  },
+  "rf-apple-jam-with-toast": {
+    "name": "Apple Jam with Toast",
+    "vendor": "rf"
+  },
+  "rf-apple-pie": {
+    "name": "Apple Pie",
+    "vendor": "rf"
+  },
+  "rf-apple-taffy": {
+    "name": "Apple Taffy",
+    "vendor": "rf"
+  },
+  "rf-baja-soda": {
+    "name": "Baja Soda",
+    "vendor": "rf"
+  },
+  "rf-baked-bread": {
+    "name": "Baked Bread",
+    "vendor": "rf"
+  },
+  "rf-baklava": {
+    "name": "Baklava",
+    "vendor": "rf"
+  },
+  "rf-banana": {
+    "name": "Banana",
+    "vendor": "rf"
+  },
+  "rf-banana-bread": {
+    "name": "Banana Bread",
+    "vendor": "rf"
+  },
+  "rf-banana-foster": {
+    "name": "Banana Foster",
+    "vendor": "rf"
+  },
+  "rf-banana-split": {
+    "name": "Banana Split",
+    "vendor": "rf"
+  },
+  "rf-banana-taffy": {
+    "name": "Banana Taffy",
+    "vendor": "rf"
+  },
+  "rf-bavarian-cream": {
+    "name": "Bavarian Cream",
+    "vendor": "rf"
+  },
+  "rf-bergamot": {
+    "name": "Bergamot",
+    "vendor": "rf"
+  },
+  "rf-black-raspberry": {
+    "name": "Black Raspberry",
+    "vendor": "rf"
+  },
+  "rf-blackberry": {
+    "name": "Blackberry",
+    "vendor": "rf"
+  },
+  "rf-blackberry-jam-with-toast": {
+    "name": "Blackberry Jam with Toast",
+    "vendor": "rf"
+  },
+  "rf-blood-orange": {
+    "name": "Blood Orange",
+    "vendor": "rf"
+  },
+  "rf-blue-energy": {
+    "name": "Blue Energy",
+    "vendor": "rf"
+  },
+  "rf-blue-moon-ice-cream": {
+    "name": "Blue Moon Ice Cream",
+    "vendor": "rf"
+  },
+  "rf-blue-raz": {
+    "name": "Blue Raz",
+    "vendor": "rf"
+  },
+  "rf-blue-raz-cotton-candy": {
+    "name": "Blue Raz Cotton Candy",
+    "vendor": "rf"
+  },
+  "rf-blue-raz-sour-candy": {
+    "name": "Blue Raz Sour Candy",
+    "vendor": "rf"
+  },
+  "rf-blue-raz-taffy": {
+    "name": "Blue Raz Taffy",
+    "vendor": "rf"
+  },
+  "rf-blueberries-and-cream": {
+    "name": "Blueberries & Cream",
+    "vendor": "rf"
+  },
+  "rf-blueberry": {
+    "name": "Blueberry",
+    "vendor": "rf"
+  },
+  "rf-blueberry-acai-pomegranate": {
+    "name": "Blueberry Acai Pomegranate",
+    "vendor": "rf"
+  },
+  "rf-blueberry-jam-with-toast": {
+    "name": "Blueberry Jam with Toast",
+    "vendor": "rf"
+  },
+  "rf-blueberry-muffin": {
+    "name": "Blueberry Muffin",
+    "vendor": "rf"
+  },
+  "rf-blueberry-pastry": {
+    "name": "Blueberry Pastry",
+    "vendor": "rf"
+  },
+  "rf-boston-cream-pie": {
+    "name": "Boston Cream Pie",
+    "vendor": "rf"
+  },
+  "rf-bourbon": {
+    "name": "Bourbon",
+    "vendor": "rf"
+  },
+  "rf-boysenberry": {
+    "name": "Boysenberry",
+    "vendor": "rf"
+  },
+  "rf-brandy": {
+    "name": "Brandy",
+    "vendor": "rf"
+  },
+  "rf-bread-pudding": {
+    "name": "Bread Pudding",
+    "vendor": "rf"
+  },
+  "rf-brown-sugar": {
+    "name": "Brown Sugar",
+    "vendor": "rf"
+  },
+  "rf-brownie": {
+    "name": "Brownie",
+    "vendor": "rf"
+  },
+  "rf-bubblegum": {
+    "name": "Bubblegum",
+    "vendor": "rf"
+  },
+  "rf-butter-cream": {
+    "name": "Butter Cream",
+    "vendor": "rf"
+  },
+  "rf-butter-crisp-candy-bar-type": {
+    "name": "Butter Crisp  Candy Bar Type",
+    "vendor": "rf"
+  },
+  "rf-butter-pecan-pie": {
+    "name": "Butter Pecan Pie",
+    "vendor": "rf"
+  },
+  "rf-butter-toffee": {
+    "name": "Butter Toffee",
+    "vendor": "rf"
+  },
+  "rf-butterscotch": {
+    "name": "Butterscotch",
+    "vendor": "rf"
+  },
+  "rf-cake-batter": {
+    "name": "Cake Batter",
+    "vendor": "rf"
+  },
+  "rf-candy-apple": {
+    "name": "Candy Apple",
+    "vendor": "rf"
+  },
+  "rf-candy-base": {
+    "name": "Candy Base",
+    "vendor": "rf"
+  },
+  "rf-caramel": {
+    "name": "Caramel",
+    "vendor": "rf"
+  },
+  "rf-caramel-apple": {
+    "name": "Caramel Apple",
+    "vendor": "rf"
+  },
+  "rf-caramel-cappuccino": {
+    "name": "Caramel Cappuccino",
+    "vendor": "rf"
+  },
+  "rf-carrot": {
+    "name": "Carrot",
+    "vendor": "rf"
+  },
+  "rf-carrot-cake": {
+    "name": "Carrot Cake",
+    "vendor": "rf"
+  },
+  "rf-champagne": {
+    "name": "Champagne",
+    "vendor": "rf"
+  },
+  "rf-cheesecake": {
+    "name": "Cheesecake",
+    "vendor": "rf"
+  },
+  "rf-cherry": {
+    "name": "Cherry",
+    "vendor": "rf"
+  },
+  "rf-cherry-cola-type": {
+    "name": "Cherry Cola Type",
+    "vendor": "rf"
+  },
+  "rf-cherry-limeade": {
+    "name": "Cherry Limeade",
+    "vendor": "rf"
+  },
+  "rf-cherry-pie": {
+    "name": "Cherry Pie",
+    "vendor": "rf"
+  },
+  "rf-cherry-taffy": {
+    "name": "Cherry Taffy",
+    "vendor": "rf"
+  },
+  "rf-chocolate": {
+    "name": "Chocolate",
+    "vendor": "rf"
+  },
+  "rf-chocolate-cake": {
+    "name": "Chocolate Cake",
+    "vendor": "rf"
+  },
+  "rf-chocolate-chip-cookie": {
+    "name": "Chocolate Chip Cookie",
+    "vendor": "rf"
+  },
+  "rf-chocolate-hazelnut-spread": {
+    "name": "Chocolate Hazelnut Spread",
+    "vendor": "rf"
+  },
+  "rf-chocolate-milkshake": {
+    "name": "Chocolate Milkshake",
+    "vendor": "rf"
+  },
+  "rf-chocolate-passion": {
+    "name": "Chocolate Passion",
+    "vendor": "rf"
+  },
+  "rf-cinnamon-custard": {
+    "name": "Cinnamon Custard",
+    "vendor": "rf"
+  },
+  "rf-cinnamon-donut": {
+    "name": "Cinnamon Donut",
+    "vendor": "rf"
+  },
+  "rf-cinnamon-roll": {
+    "name": "Cinnamon Roll",
+    "vendor": "rf"
+  },
+  "rf-cinnamon-toast": {
+    "name": "Cinnamon Toast",
+    "vendor": "rf"
+  },
+  "rf-coconut": {
+    "name": "Coconut",
+    "vendor": "rf"
+  },
+  "rf-coconut-cream-pie": {
+    "name": "Coconut Cream Pie",
+    "vendor": "rf"
+  },
+  "rf-coconut-spiced-rum": {
+    "name": "Coconut Spiced Rum",
+    "vendor": "rf"
+  },
+  "rf-coffee": {
+    "name": "Coffee",
+    "vendor": "rf"
+  },
+  "rf-coffee-cake": {
+    "name": "Coffee Cake",
+    "vendor": "rf"
+  },
+  "rf-cola-type": {
+    "name": "Cola Type",
+    "vendor": "rf"
+  },
+  "rf-condensed-milk": {
+    "name": "Condensed Milk",
+    "vendor": "rf"
+  },
+  "rf-cookie-dough": {
+    "name": "Cookie Dough",
+    "vendor": "rf"
+  },
+  "rf-cookies-and-cream": {
+    "name": "Cookies & Cream",
+    "vendor": "rf"
+  },
+  "rf-cotton-candy": {
+    "name": "Cotton Candy",
+    "vendor": "rf"
+  },
+  "rf-cream": {
+    "name": "Cream",
+    "vendor": "rf"
+  },
+  "rf-cream-cheese": {
+    "name": "Cream Cheese",
+    "vendor": "rf"
+  },
+  "rf-cream-soda": {
+    "name": "Cream Soda",
+    "vendor": "rf"
+  },
+  "rf-creme-brulee": {
+    "name": "Creme Brulee",
+    "vendor": "rf"
+  },
+  "rf-cucumber": {
+    "name": "Cucumber",
+    "vendor": "rf"
+  },
+  "rf-deep-fried-cookies-and-cream": {
+    "name": "Deep Fried Cookies and Cream",
+    "vendor": "rf"
+  },
+  "rf-deep-fried-spongecake": {
+    "name": "Deep Fried Spongecake",
+    "vendor": "rf"
+  },
+  "rf-dreamsicle": {
+    "name": "Dreamsicle",
+    "vendor": "rf"
+  },
+  "rf-dulce-de-leche": {
+    "name": "Dulce de Leche",
+    "vendor": "rf"
+  },
+  "rf-elephant-ear": {
+    "name": "Elephant Ear",
+    "vendor": "rf"
+  },
+  "rf-french-toast": {
+    "name": "French Toast",
+    "vendor": "rf"
+  },
+  "rf-french-vanilla": {
+    "name": "French Vanilla",
+    "vendor": "rf"
+  },
+  "rf-french-vanilla-cappuccino": {
+    "name": "French Vanilla Cappuccino",
+    "vendor": "rf"
+  },
+  "rf-fried-chicken": {
+    "name": "Fried Chicken",
+    "vendor": "rf"
+  },
+  "rf-fried-dough": {
+    "name": "Fried Dough",
+    "vendor": "rf"
+  },
+  "rf-frosted-animal-cookie": {
+    "name": "Frosted Animal Cookie",
+    "vendor": "rf"
+  },
+  "rf-frosted-cereal": {
+    "name": "Frosted Cereal",
+    "vendor": "rf"
+  },
+  "rf-fruit-cake": {
+    "name": "Fruit Cake",
+    "vendor": "rf"
+  },
+  "rf-fruit-candy": {
+    "name": "Fruit Candy",
+    "vendor": "rf"
+  },
+  "rf-fruit-danish": {
+    "name": "Fruit Danish",
+    "vendor": "rf"
+  },
+  "rf-fruit-pebbles": {
+    "name": "Fruit Pebbles",
+    "vendor": "rf"
+  },
+  "rf-fruity-circles": {
+    "name": "Fruity Circles",
+    "vendor": "rf"
+  },
+  "rf-funnel-cake": {
+    "name": "Funnel Cake",
+    "vendor": "rf"
+  },
+  "rf-ginger-ale": {
+    "name": "Ginger Ale",
+    "vendor": "rf"
+  },
+  "rf-gingerbread-cookie": {
+    "name": "Gingerbread Cookie",
+    "vendor": "rf"
+  },
+  "rf-golden-cereal": {
+    "name": "Golden Cereal",
+    "vendor": "rf"
+  },
+  "rf-graham-cracker": {
+    "name": "Graham Cracker",
+    "vendor": "rf"
+  },
+  "rf-grape": {
+    "name": "Grape",
+    "vendor": "rf"
+  },
+  "rf-grape-taffy": {
+    "name": "Grape Taffy",
+    "vendor": "rf"
+  },
+  "rf-grapefruit": {
+    "name": "Grapefruit",
+    "vendor": "rf"
+  },
+  "rf-green-energy": {
+    "name": "Green Energy",
+    "vendor": "rf"
+  },
+  "rf-guava": {
+    "name": "Guava",
+    "vendor": "rf"
+  },
+  "rf-gummy-candy": {
+    "name": "Gummy Candy",
+    "vendor": "rf"
+  },
+  "rf-hazelnut": {
+    "name": "Hazelnut",
+    "vendor": "rf"
+  },
+  "rf-honey": {
+    "name": "Honey",
+    "vendor": "rf"
+  },
+  "rf-honeydew": {
+    "name": "Honeydew",
+    "vendor": "rf"
+  },
+  "rf-hot-cocoa": {
+    "name": "Hot Cocoa",
+    "vendor": "rf"
+  },
+  "rf-ice-cream-sandwich": {
+    "name": "Ice Cream Sandwich",
+    "vendor": "rf"
+  },
+  "rf-key-lime-pie": {
+    "name": "Key Lime Pie",
+    "vendor": "rf"
+  },
+  "rf-kiwi": {
+    "name": "Kiwi",
+    "vendor": "rf"
+  },
+  "rf-lemon": {
+    "name": "Lemon",
+    "vendor": "rf"
+  },
+  "rf-lemon-cake": {
+    "name": "Lemon Cake",
+    "vendor": "rf"
+  },
+  "rf-lemon-cookie": {
+    "name": "Lemon Cookie",
+    "vendor": "rf"
+  },
+  "rf-lemon-meringue": {
+    "name": "Lemon Meringue",
+    "vendor": "rf"
+  },
+  "rf-lemon-tea": {
+    "name": "Lemon Tea",
+    "vendor": "rf"
+  },
+  "rf-lemonade": {
+    "name": "Lemonade",
+    "vendor": "rf"
+  },
+  "rf-lime": {
+    "name": "Lime",
+    "vendor": "rf"
+  },
+  "rf-lychee": {
+    "name": "Lychee",
+    "vendor": "rf"
+  },
+  "rf-mango": {
+    "name": "Mango",
+    "vendor": "rf"
+  },
+  "rf-marshmallow": {
+    "name": "Marshmallow",
+    "vendor": "rf"
+  },
+  "rf-marshmallow-candy": {
+    "name": "Marshmallow Candy",
+    "vendor": "rf"
+  },
+  "rf-masala-chai": {
+    "name": "Masala Chai",
+    "vendor": "rf"
+  },
+  "rf-melon-medley": {
+    "name": "Melon Medley",
+    "vendor": "rf"
+  },
+  "rf-meringue": {
+    "name": "Meringue",
+    "vendor": "rf"
+  },
+  "rf-milk": {
+    "name": "Milk",
+    "vendor": "rf"
+  },
+  "rf-mimosa": {
+    "name": "Mimosa",
+    "vendor": "rf"
+  },
+  "rf-mint": {
+    "name": "Mint",
+    "vendor": "rf"
+  },
+  "rf-mixed-berries": {
+    "name": "Mixed Berries",
+    "vendor": "rf"
+  },
+  "rf-mocha": {
+    "name": "Mocha",
+    "vendor": "rf"
+  },
+  "rf-mojito": {
+    "name": "Mojito",
+    "vendor": "rf"
+  },
+  "rf-molasses": {
+    "name": "Molasses",
+    "vendor": "rf"
+  },
+  "rf-mtn-juice": {
+    "name": "MTN Juice",
+    "vendor": "rf"
+  },
+  "rf-neapolitan-ice-cream": {
+    "name": "Neapolitan Ice Cream",
+    "vendor": "rf"
+  },
+  "rf-nutmeg": {
+    "name": "Nutmeg",
+    "vendor": "rf"
+  },
+  "rf-oatmeal-cookie": {
+    "name": "Oatmeal Cookie",
+    "vendor": "rf"
+  },
+  "rf-orange": {
+    "name": "Orange",
+    "vendor": "rf"
+  },
+  "rf-orange-candy-burst-type": {
+    "name": "Orange Candy Burst Type",
+    "vendor": "rf"
+  },
+  "rf-orange-cream": {
+    "name": "Orange Cream",
+    "vendor": "rf"
+  },
+  "rf-paczki": {
+    "name": "Paczki",
+    "vendor": "rf"
+  },
+  "rf-pancakes": {
+    "name": "Pancakes",
+    "vendor": "rf"
+  },
+  "rf-passion-fruit": {
+    "name": "Passion Fruit",
+    "vendor": "rf"
+  },
+  "rf-peach": {
+    "name": "Peach",
+    "vendor": "rf"
+  },
+  "rf-peach-cobbler": {
+    "name": "Peach Cobbler",
+    "vendor": "rf"
+  },
+  "rf-peaches-and-cream": {
+    "name": "Peaches & Cream",
+    "vendor": "rf"
+  },
+  "rf-peachy-mint": {
+    "name": "Peachy Mint",
+    "vendor": "rf"
+  },
+  "rf-peanut-brittle": {
+    "name": "Peanut Brittle",
+    "vendor": "rf"
+  },
+  "rf-peanut-butter": {
+    "name": "Peanut Butter",
+    "vendor": "rf"
+  },
+  "rf-peanut-butter-cookie": {
+    "name": "Peanut Butter Cookie",
+    "vendor": "rf"
+  },
+  "rf-peanut-butter-cup": {
+    "name": "Peanut Butter Cup",
+    "vendor": "rf"
+  },
+  "rf-pear": {
+    "name": "Pear",
+    "vendor": "rf"
+  },
+  "rf-pina-colada": {
+    "name": "Pina Colada",
+    "vendor": "rf"
+  },
+  "rf-pineapple": {
+    "name": "Pineapple",
+    "vendor": "rf"
+  },
+  "rf-pineapple-cake": {
+    "name": "Pineapple Cake",
+    "vendor": "rf"
+  },
+  "rf-pink-candy-burst-type": {
+    "name": "Pink Candy Burst Type",
+    "vendor": "rf"
+  },
+  "rf-pistachio": {
+    "name": "Pistachio",
+    "vendor": "rf"
+  },
+  "rf-pomegranate": {
+    "name": "Pomegranate",
+    "vendor": "rf"
+  },
+  "rf-pumpkin-pie": {
+    "name": "Pumpkin Pie",
+    "vendor": "rf"
+  },
+  "rf-rainbow-candy": {
+    "name": "Rainbow Candy",
+    "vendor": "rf"
+  },
+  "rf-rainbow-sherbet": {
+    "name": "Rainbow Sherbet",
+    "vendor": "rf"
+  },
+  "rf-raspberry": {
+    "name": "Raspberry",
+    "vendor": "rf"
+  },
+  "rf-red-candy-burst-type": {
+    "name": "Red Candy Burst Type",
+    "vendor": "rf"
+  },
+  "rf-red-energy": {
+    "name": "Red Energy",
+    "vendor": "rf"
+  },
+  "rf-red-hot-cinnamon": {
+    "name": "Red Hot Cinnamon",
+    "vendor": "rf"
+  },
+  "rf-red-soda": {
+    "name": "Red Soda",
+    "vendor": "rf"
+  },
+  "rf-red-white-and-blue-popsicle": {
+    "name": "Red, White, & Blue Popsicle",
+    "vendor": "rf"
+  },
+  "rf-rhubarb": {
+    "name": "Rhubarb",
+    "vendor": "rf"
+  },
+  "rf-rice-crispy-treat": {
+    "name": "Rice Crispy Treat",
+    "vendor": "rf"
+  },
+  "rf-rice-pudding": {
+    "name": "Rice Pudding",
+    "vendor": "rf"
+  },
+  "rf-rocky-road-ice-cream": {
+    "name": "Rocky Road Ice Cream",
+    "vendor": "rf"
+  },
+  "rf-root-beer-float": {
+    "name": "Root Beer Float",
+    "vendor": "rf"
+  },
+  "rf-root-beer-fountain-style": {
+    "name": "Root Beer Fountain Style",
+    "vendor": "rf"
+  },
+  "rf-rose": {
+    "name": "Rose",
+    "vendor": "rf"
+  },
+  "rf-s-mores": {
+    "name": "S'mores",
+    "vendor": "rf"
+  },
+  "rf-samoa-cookie-type": {
+    "name": "Samoa Cookie Type",
+    "vendor": "rf"
+  },
+  "rf-sangria": {
+    "name": "Sangria",
+    "vendor": "rf"
+  },
+  "rf-shamrock-shake": {
+    "name": "Shamrock Shake",
+    "vendor": "rf"
+  },
+  "rf-shortbread-cookie": {
+    "name": "Shortbread Cookie",
+    "vendor": "rf"
+  },
+  "rf-silly-rabbit": {
+    "name": "Silly Rabbit",
+    "vendor": "rf"
+  },
+  "rf-snickerdoodle": {
+    "name": "Snickerdoodle",
+    "vendor": "rf"
+  },
+  "rf-soda-base": {
+    "name": "Soda Base",
+    "vendor": "rf"
+  },
+  "rf-souffle": {
+    "name": "Souffle",
+    "vendor": "rf"
+  },
+  "rf-sour-gummy": {
+    "name": "Sour Gummy",
+    "vendor": "rf"
+  },
+  "rf-spearmint": {
+    "name": "Spearmint",
+    "vendor": "rf"
+  },
+  "rf-sponge-cake": {
+    "name": "Sponge Cake",
+    "vendor": "rf"
+  },
+  "rf-sriracha": {
+    "name": "Sriracha",
+    "vendor": "rf"
+  },
+  "rf-strawberries-and-cream": {
+    "name": "Strawberries & Cream",
+    "vendor": "rf"
+  },
+  "rf-strawberry": {
+    "name": "Strawberry",
+    "vendor": "rf"
+  },
+  "rf-strawberry-banana-smoothie": {
+    "name": "Strawberry Banana Smoothie",
+    "vendor": "rf"
+  },
+  "rf-strawberry-gelato": {
+    "name": "Strawberry Gelato",
+    "vendor": "rf"
+  },
+  "rf-strawberry-jam-with-toast": {
+    "name": "Strawberry Jam with Toast",
+    "vendor": "rf"
+  },
+  "rf-strawberry-kiwi": {
+    "name": "Strawberry Kiwi",
+    "vendor": "rf"
+  },
+  "rf-strawberry-kiwi-taffy": {
+    "name": "Strawberry Kiwi Taffy",
+    "vendor": "rf"
+  },
+  "rf-strawberry-lemonade": {
+    "name": "Strawberry Lemonade",
+    "vendor": "rf"
+  },
+  "rf-strawberry-milkshake": {
+    "name": "Strawberry Milkshake",
+    "vendor": "rf"
+  },
+  "rf-strawberry-pastry": {
+    "name": "Strawberry Pastry",
+    "vendor": "rf"
+  },
+  "rf-strawberry-peach": {
+    "name": "Strawberry Peach",
+    "vendor": "rf"
+  },
+  "rf-strawberry-rhubarb": {
+    "name": "Strawberry Rhubarb",
+    "vendor": "rf"
+  },
+  "rf-strawberry-taffy": {
+    "name": "Strawberry Taffy",
+    "vendor": "rf"
+  },
+  "rf-stuffed-french-toast": {
+    "name": "Stuffed French Toast",
+    "vendor": "rf"
+  },
+  "rf-sugar-cookie": {
+    "name": "Sugar Cookie",
+    "vendor": "rf"
+  },
+  "rf-sugar-cub": {
+    "name": "Sugar Cub",
+    "vendor": "rf"
+  },
+  "rf-sugar-plum": {
+    "name": "Sugar Plum",
+    "vendor": "rf"
+  },
+  "rf-super-ice-cream": {
+    "name": "Super Ice Cream",
+    "vendor": "rf"
+  },
+  "rf-swedish-gummy": {
+    "name": "Swedish Gummy",
+    "vendor": "rf"
+  },
+  "rf-sweet-cream": {
+    "name": "Sweet Cream",
+    "vendor": "rf"
+  },
+  "rf-sweet-potato": {
+    "name": "Sweet Potato",
+    "vendor": "rf"
+  },
+  "rf-sweet-potato-pie": {
+    "name": "Sweet Potato Pie",
+    "vendor": "rf"
+  },
+  "rf-sweet-tart-candy": {
+    "name": "Sweet Tart Candy",
+    "vendor": "rf"
+  },
+  "rf-taffy-base": {
+    "name": "Taffy Base",
+    "vendor": "rf"
+  },
+  "rf-tomato": {
+    "name": "Tomato",
+    "vendor": "rf"
+  },
+  "rf-tropical-punch": {
+    "name": "Tropical Punch",
+    "vendor": "rf"
+  },
+  "rf-tropical-rainbow-candy": {
+    "name": "Tropical Rainbow Candy",
+    "vendor": "rf"
+  },
+  "rf-vanilla-custard": {
+    "name": "Vanilla Custard",
+    "vendor": "rf"
+  },
+  "rf-vanilla-ice-cream": {
+    "name": "Vanilla Ice Cream",
+    "vendor": "rf"
+  },
+  "rf-warm-apple-cider": {
+    "name": "Warm Apple Cider",
+    "vendor": "rf"
+  },
+  "rf-watermelon": {
+    "name": "Watermelon",
+    "vendor": "rf"
+  },
+  "rf-watermelon-taffy": {
+    "name": "Watermelon Taffy",
+    "vendor": "rf"
+  },
+  "rf-white-chocolate": {
+    "name": "White Chocolate",
+    "vendor": "rf"
+  },
+  "rf-wild-cherry": {
+    "name": "Wild Cherry",
+    "vendor": "rf"
+  },
+  "rf-yellow-candy-burst-type": {
+    "name": "Yellow Candy Burst Type",
+    "vendor": "rf"
+  },
+  "rf-yogurt": {
+    "name": "Yogurt",
+    "vendor": "rf"
+  },
+  "rf-yogurt-parfait": {
+    "name": "Yogurt Parfait",
+    "vendor": "rf"
+  },
+  "rf-yumberry": {
+    "name": "Yumberry",
+    "vendor": "rf"
+  },
+};
---
+++ src/data/flavors-sc.js
@@ -0,0 +1,234 @@
+const staticDataFlavorsSc = {
+  "sc-555": {
+    "name": "555",
+    "vendor": "sc"
+  },
+  "sc-almond": {
+    "name": "Almond",
+    "vendor": "sc"
+  },
+  "sc-american-red": {
+    "name": "American Red",
+    "vendor": "sc"
+  },
+  "sc-arabic-flavoring": {
+    "name": "Arabic Flavoring",
+    "vendor": "sc"
+  },
+  "sc-b-and-h": {
+    "name": "B & H",
+    "vendor": "sc"
+  },
+  "sc-banana-cream-pie": {
+    "name": "Banana Cream Pie",
+    "vendor": "sc"
+  },
+  "sc-banana": {
+    "name": "Banana",
+    "vendor": "sc"
+  },
+  "sc-black-mile": {
+    "name": "Black Mile",
+    "vendor": "sc"
+  },
+  "sc-blackberry": {
+    "name": "Blackberry",
+    "vendor": "sc"
+  },
+  "sc-blended-flavoring": {
+    "name": "Blended Flavoring",
+    "vendor": "sc"
+  },
+  "sc-blueberry": {
+    "name": "Blueberry",
+    "vendor": "sc"
+  },
+  "sc-c-style": {
+    "name": "C-Style",
+    "vendor": "sc"
+  },
+  "sc-cappuccino": {
+    "name": "Cappuccino",
+    "vendor": "sc"
+  },
+  "sc-caramel-candy": {
+    "name": "Caramel Candy",
+    "vendor": "sc"
+  },
+  "sc-caramel-cream-puff": {
+    "name": "Caramel Cream Puff",
+    "vendor": "sc"
+  },
+  "sc-caramel-mocha": {
+    "name": "Caramel Mocha",
+    "vendor": "sc"
+  },
+  "sc-cherry-cigar": {
+    "name": "Cherry Cigar",
+    "vendor": "sc"
+  },
+  "sc-cigar": {
+    "name": "Cigar",
+    "vendor": "sc"
+  },
+  "sc-coconut": {
+    "name": "Coconut",
+    "vendor": "sc"
+  },
+  "sc-cohiba": {
+    "name": "Cohiba",
+    "vendor": "sc"
+  },
+  "sc-cubana": {
+    "name": "Cubana",
+    "vendor": "sc"
+  },
+  "sc-dragon-fruit": {
+    "name": "Dragon Fruit",
+    "vendor": "sc"
+  },
+  "sc-dunhill": {
+    "name": "Dunhill",
+    "vendor": "sc"
+  },
+  "sc-espresso": {
+    "name": "Espresso",
+    "vendor": "sc"
+  },
+  "sc-french-pipe": {
+    "name": "French Pipe",
+    "vendor": "sc"
+  },
+  "sc-grape": {
+    "name": "Grape",
+    "vendor": "sc"
+  },
+  "sc-havana-dry": {
+    "name": "Havana Dry",
+    "vendor": "sc"
+  },
+  "sc-hazelnut": {
+    "name": "Hazelnut",
+    "vendor": "sc"
+  },
+  "sc-honey-flue-cured": {
+    "name": "Honey Flue Cured",
+    "vendor": "sc"
+  },
+  "sc-island-flavoring": {
+    "name": "Island Flavoring",
+    "vendor": "sc"
+  },
+  "sc-m-style": {
+    "name": "M-Style",
+    "vendor": "sc"
+  },
+  "sc-mandarin": {
+    "name": "Mandarin",
+    "vendor": "sc"
+  },
+  "sc-mango": {
+    "name": "Mango",
+    "vendor": "sc"
+  },
+  "sc-maxx-blend": {
+    "name": "Maxx Blend",
+    "vendor": "sc"
+  },
+  "sc-melon": {
+    "name": "Melon",
+    "vendor": "sc"
+  },
+  "sc-milk-chocolate": {
+    "name": "Milk Chocolate",
+    "vendor": "sc"
+  },
+  "sc-old-captain": {
+    "name": "Old Captain",
+    "vendor": "sc"
+  },
+  "sc-old-cherry": {
+    "name": "Old Cherry",
+    "vendor": "sc"
+  },
+  "sc-parlay": {
+    "name": "Parlay",
+    "vendor": "sc"
+  },
+  "sc-peach": {
+    "name": "Peach",
+    "vendor": "sc"
+  },
+  "sc-pineapple": {
+    "name": "Pineapple",
+    "vendor": "sc"
+  },
+  "sc-raspberry": {
+    "name": "Raspberry",
+    "vendor": "sc"
+  },
+  "sc-real-cig": {
+    "name": "Real Cig",
+    "vendor": "sc"
+  },
+  "sc-red-apple": {
+    "name": "Red Apple",
+    "vendor": "sc"
+  },
+  "sc-red-energy": {
+    "name": "Red Energy",
+    "vendor": "sc"
+  },
+  "sc-ruyan": {
+    "name": "Ruyan",
+    "vendor": "sc"
+  },
+  "sc-ry-4": {
+    "name": "RY-4",
+    "vendor": "sc"
+  },
+  "sc-seven-stars": {
+    "name": "Seven Stars",
+    "vendor": "sc"
+  },
+  "sc-seven": {
+    "name": "Seven",
+    "vendor": "sc"
+  },
+  "sc-shisha": {
+    "name": "Shisha",
+    "vendor": "sc"
+  },
+  "sc-strawberry": {
+    "name": "Strawberry",
+    "vendor": "sc"
+  },
+  "sc-tropical-fruit": {
+    "name": "Tropical Fruit",
+    "vendor": "sc"
+  },
+  "sc-turkish": {
+    "name": "Turkish",
+    "vendor": "sc"
+  },
+  "sc-tutti-fruit": {
+    "name": "Tutti Fruit",
+    "vendor": "sc"
+  },
+  "sc-usa-mix": {
+    "name": "USA Mix",
+    "vendor": "sc"
+  },
+  "sc-virginia-flue-cured": {
+    "name": "Virginia Flue Cured",
+    "vendor": "sc"
+  },
+  "sc-wenston": {
+    "name": "Wenston",
+    "vendor": "sc"
+  },
+  "sc-western-flavoring": {
+    "name": "Western Flavoring",
+    "vendor": "sc"
+  },
+};
