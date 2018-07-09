12becb32e695f3f093d23df86961b3171bb87031
Simon Fuhrmann <simonfuhrmann@gmx.de>
2018-07-08 21:30:26 -0700

    Add WS-3, 4, 12 generic cooling agents

--- src/data/flavors-misc.js
+++ src/data/flavors-misc.js
@@ -1,6 +1,18 @@
 const staticDataFlavorsMisc = {
+  "misc-ws-12-cooling-agent": {
+    "name": "WS-12 (Cooling Agent)",
+    "vendor": "misc"
+  },
   "misc-ws-23-cooling-agent": {
     "name": "WS-23 (Cooling Agent)",
+    "vendor": "misc"
+  },
+  "misc-ws-3-cooling-agent": {
+    "name": "WS-3 (Cooling Agent)",
+    "vendor": "misc"
+  },
+  "misc-ws-5-cooling-agent": {
+    "name": "WS-5 (Cooling Agent)",
     "vendor": "misc"
   },
 };
